192713263374215065da723061e9d28a6ab89371
cloudawan <edcyc.tw@gmail.com>
2016-11-06 13:36:29 +0800

    Apt-get update

--- cloud-automation.sh
+++ cloud-automation.sh
@@ -3,6 +3,7 @@
 #sudo apt-add-repository ppa:ansible/ansible -y
 #sudo apt-get update
 #sudo apt-get install -y --force-yes ansible
+sudo apt-get update
 sudo apt-get install -y python-pip
 sudo pip install ansible==2.1.2
 
